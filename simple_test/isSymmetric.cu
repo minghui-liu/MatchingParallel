5538110243f0b8dbb30db129b3f4928ade07f718
Kevin Liu <minghui.liu@trincoll.edu>
2014-06-06 11:38:23 -0400

    simple_test

---
+++ simple_test/isSymmetric.cu
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BLOCK_SIZE 32
+
+
+// Matrices are stored in row-major order:
+// M(row, col) = *(M.elements + row * M.width + col)
+typedef struct {
+  int width;
+  int height;
+	double* elements;
+} Matrix;
+
+// check if a square matrix is symmetric
+__global__
+void isSymmetricKernel(Matrix d_A, int *d_result) {
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	if(row >= d_A.height || col >= d_A.width) return;
+	if(d_A.elements[row*d_A.width+col] != d_A.elements[row + col*d_A.width]) {
+		printf("%d, %d  not equal\n", row, col);
+		*(d_result) = 0;
+	}
+}
+
+int isSymmetric(Matrix A) {
+	printf("isSymmetric()\n");
+	// load A to device memory
+	Matrix d_A;
+	d_A.width = A.width;
+	d_A.height = A.height;
+	size_t size = A.width * A.height * sizeof(double);
+	cudaError_t err = cudaMalloc(&d_A.elements, size);
+	printf("CUDA malloc A: %s\n", cudaGetErrorString(err));	
+	cudaMemcpy(d_A.elements, A.elements, size, cudaMemcpyHostToDevice);
+	printf("Copy A to device: %s\n", cudaGetErrorString(err));
+
+	// load result to device memory
+	int result = 1;
+	int *d_result;
+	err = cudaMalloc(&d_result, sizeof(int));
+	printf("CUDA malloc d_result: %s\n", cudaGetErrorString(err));	
+	cudaMemcpy(d_result, &result, sizeof(int), cudaMemcpyHostToDevice);	
+	printf("Copy result to device: %s\n", cudaGetErrorString(err));
+
+	// invoke kernel
+	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+	dim3 dimGrid( (A.width + dimBlock.x - 1)/dimBlock.x, (A.height + dimBlock.y - 1)/dimBlock.y );
+	isSymmetricKernel<<<dimGrid, dimBlock>>>(d_A, d_result);
+	err = cudaThreadSynchronize();
+	printf("Run kernel: %s\n", cudaGetErrorString(err));
+
+	//read result from fdevice memory
+	err = cudaMemcpy(&result, d_result, sizeof(int), cudaMemcpyDeviceToHost);
+	printf("Copy result off of device: %s\n",cudaGetErrorString(err));
+
+	// free device memory
+	cudaFree(d_A.elements);
+	cudaFree(d_result);
+
+	return result;
+}
+
+
+void printMatrix(Matrix A) {
+	printf("\n");
+	for (int i=0; i<A.height; i++) {
+		for (int j=0; j<A.width; j++) {
+			printf("%.4f ", A.elements[i*A.width+j]); 
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+
+int main() {
+	srand(time(0));	
+	Matrix A;
+	A.height = 3;
+	A.width = 3;
+	A.elements = (double*)malloc(A.width * A.height * sizeof(double));
+	double AE[3][3] = {{0,1,2},{1,0,3},{2,3,0}};
+	memcpy(A.elements, AE, A.height*A.width*sizeof(double));
+	// call isSymmetric
+	int result =	isSymmetric(A);
+	printMatrix(A);
+	printf("result: %d \n", result);
+}
