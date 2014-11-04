ea0d4502fd6ebaf6ca61963543eb109b4ccae972
Kevin Liu <minghui.liu@trincoll.edu>
2014-11-04 12:41:03 -0500

    Reduce the memory usage of each thread in child grid, largest test size is now 80

--- graphMatching.cu
+++ graphMatching.cu
@@ -16,7 +16,126 @@ void expKernel(Matrix d_D, double sigma) {
 }
 
 __global__
+void calculateD(Matrix d_G1_col, Matrix d_G2t_row, Matrix d_D) {
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	// if out of boundary return
+	if (y >= d_D.height || x >= d_D.width) return;
+	
+	d_D.elements[y * d_D.width + x] = d_G1_col.elements[y] - d_G2t_row.elements[x];
+}
+
+__global__
+void expDkernel(Matrix d_G1_col, Matrix d_G2t_row, Matrix d_Y, double sigma) {
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	if(row >= d_Y.height || col >= d_Y.width) return;
+	//int idx = row * d_Y.width + col;
+	double d = d_G1_col.elements[row] - d_G2t_row.elements[col];
+	double e = exp(-d * d / sigma);
+	d_Y.elements[row * d_Y.width + col] += e;
+}
+
+__global__
 void marginalize(Matrix d_G1, Matrix d_G2t, double sigma, Matrix d_Y) {
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	// if out of boundary return
+	if (y >= d_Y.height || x >= d_Y.width) return;
+
+	// create d_G1_col and d_G2_row	
+	Matrix d_G1_col, d_G2t_row;
+	d_G1_col.height = d_G1.height;
+	d_G1_col.width = 1;
+	size_t size = d_G1_col.width * d_G1_col.height * sizeof(double);
+	cudaError_t err = cudaMalloc(&d_G1_col.elements, size);
+	printf("CUDA malloc d_G1_col: %s\n", cudaGetErrorString(err));
+	d_G2t_row.height = 1;
+	d_G2t_row.width = d_G2t.width;
+	size = d_G2t_row.width * d_G2t_row.height * sizeof(double);
+	err = cudaMalloc(&d_G2t_row.elements, size);
+	printf("CUDA malloc d_G2t_row: %s\n", cudaGetErrorString(err));
+
+	// calculate D
+
+	// G1(:,i) invoke getCol kernel
+	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+	dim3 dimGrid( (d_G1.width + dimBlock.x - 1)/dimBlock.x, (d_G1.height + dimBlock.y - 1)/dimBlock.y );
+	getColKernel<<<dimGrid, dimBlock>>>(d_G1, d_G1_col, y);
+	
+	// G2t(j,:) invoke getRow kernel
+	dimGrid = dim3( (d_G2t.width + dimBlock.x - 1)/dimBlock.x, (d_G2t.height + dimBlock.y - 1)/dimBlock.y );
+	getRowKernel<<<dimGrid, dimBlock>>>(d_G2t, d_G2t_row, x);
+
+	// calculate d_D
+	dimGrid = dim3( (d_Y.width + dimBlock.x - 1)/dimBlock.x, (d_Y.height + dimBlock.y - 1)/dimBlock.y ); 
+	expDkernel<<<dimGrid, dimBlock>>>(d_G1_col, d_G2t_row, d_Y, sigma);	
+
+	// free memory space
+	cudaFree(d_G1_col.elements);
+	cudaFree(d_G2t_row.elements);
+
+}
+	
+__global__
+void marginalize1(Matrix d_G1, Matrix d_G2t, double sigma, Matrix d_Y) {
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	// if out of boundary return
+	if (y >= d_Y.height || x >= d_Y.width) return;
+	
+	// create d_D
+	Matrix d_D;
+	d_D.height = d_Y.height;
+	d_D.width = d_Y.width;
+	size_t size = d_D.width * d_D.height * sizeof(double);
+	cudaError_t err = cudaMalloc(&d_D.elements, size);
+	printf("CUDA malloc d_D: %s\n", cudaGetErrorString(err));
+	
+	// create d_G1_col and d_G2_row	
+	Matrix d_G1_col, d_G2t_row;
+	d_G1_col.height = d_G1.height;
+	d_G1_col.width = 1;
+	size = d_G1_col.width * d_G1_col.height * sizeof(double);
+	err = cudaMalloc(&d_G1_col.elements, size);
+	printf("CUDA malloc d_G1_col: %s\n", cudaGetErrorString(err));
+	d_G2t_row.height = 1;
+	d_G2t_row.width = d_G2t.width;
+	size = d_G2t_row.width * d_G2t_row.height * sizeof(double);
+	err = cudaMalloc(&d_G2t_row.elements, size);
+	printf("CUDA malloc d_G2t_row: %s\n", cudaGetErrorString(err));
+
+	// calculate D
+
+	// G1(:,i) invoke getCol kernel
+	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+	dim3 dimGrid( (d_G1.width + dimBlock.x - 1)/dimBlock.x, (d_G1.height + dimBlock.y - 1)/dimBlock.y );
+	getColKernel<<<dimGrid, dimBlock>>>(d_G1, d_G1_col, y);
+	
+	// G2t(j,:) invoke getRow kernel
+	dimGrid = dim3( (d_G2t.width + dimBlock.x - 1)/dimBlock.x, (d_G2t.height + dimBlock.y - 1)/dimBlock.y );
+	getRowKernel<<<dimGrid, dimBlock>>>(d_G2t, d_G2t_row, x);
+
+	// calculate d_D
+	dimGrid = dim3( (d_D.width + dimBlock.x - 1)/dimBlock.x, (d_D.height + dimBlock.y - 1)/dimBlock.y ); 
+	calculateD<<<dimGrid, dimBlock>>>(d_G1_col, d_G2t_row, d_D);	
+	
+	// exp((-d.*d)./sigma) invoke exp kernel
+	expKernel<<<dimGrid, dimBlock>>>(d_D, sigma);
+	
+	// write to Y
+	matAddKernel<<<dimGrid, dimBlock>>>(d_Y, d_D, d_Y);
+
+	// free memory space
+	cudaFree(d_G1_col.elements);
+	cudaFree(d_G2t_row.elements);
+	cudaFree(d_D.elements);
+
+}
+
+
+__global__
+void marginalize0(Matrix d_G1, Matrix d_G2t, double sigma, Matrix d_Y) {
 	int y = blockIdx.y * blockDim.y + threadIdx.y;
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
 	// if out of boundary return
