e49519d2e1b18c51fc126bd865dc371218ee8317
Kevin Liu <minghui.liu@trincoll.edu>
2014-11-10 00:22:50 -0500

    Abandon Dynamic Parellism and use a single level parelle Margianlize kernel

--- graphMatching.cu
+++ graphMatching.cu
@@ -5,208 +5,8 @@
 
 #define BLOCK_SIZE 32
 
-// exp kernel
 __global__
-void expKernel(Matrix d_D, float sigma) {
-	int col = blockIdx.y * blockDim.y + threadIdx.y;
-	int row = blockIdx.x * blockDim.x + threadIdx.x;
-	if(row >= d_D.height || col >= d_D.width) return;
-	int idx = row * d_D.width + col;
-	d_D.elements[idx] = exp(-d_D.elements[idx] * d_D.elements[idx] / sigma);
-}
-
-__global__
-void calculateD(Matrix d_G1_col, Matrix d_G2t_row, Matrix d_D) {
-	int y = blockIdx.y * blockDim.y + threadIdx.y;
-	int x = blockIdx.x * blockDim.x + threadIdx.x;
-	// if out of boundary return
-	if (y >= d_D.height || x >= d_D.width) return;
-	
-	d_D.elements[y * d_D.width + x] = d_G1_col.elements[y] - d_G2t_row.elements[x];
-}
-
-__global__
-void expDkernel(Matrix d_G1_col, Matrix d_G2t_row, Matrix d_Y, float sigma) {
-	int row = blockIdx.y * blockDim.y + threadIdx.y;
-	int col = blockIdx.x * blockDim.x + threadIdx.x;
-	if(row >= d_Y.height || col >= d_Y.width) return;
-	//int idx = row * d_Y.width + col;
-	float d = d_G1_col.elements[row] - d_G2t_row.elements[col];
-	float e = exp(-d * d / sigma);
-	d_Y.elements[row * d_Y.width + col] += e;
-}
-
-__global__
-void marginalize(Matrix d_G1, Matrix d_G2t, float sigma, Matrix d_Y) {
-	int y = blockIdx.y * blockDim.y + threadIdx.y;
-	int x = blockIdx.x * blockDim.x + threadIdx.x;
-	// if out of boundary return
-	if (y >= d_Y.height || x >= d_Y.width) return;
-
-	// create d_G1_col and d_G2_row	
-	Matrix d_G1_col, d_G2t_row;
-	d_G1_col.height = d_G1.height;
-	d_G1_col.width = 1;
-	size_t size = d_G1_col.width * d_G1_col.height * sizeof(float);
-	cudaError_t err = cudaMalloc(&d_G1_col.elements, size);
-	printf("CUDA malloc d_G1_col: %s\n", cudaGetErrorString(err));
-	d_G2t_row.height = 1;
-	d_G2t_row.width = d_G2t.width;
-	size = d_G2t_row.width * d_G2t_row.height * sizeof(float);
-	err = cudaMalloc(&d_G2t_row.elements, size);
-	printf("CUDA malloc d_G2t_row: %s\n", cudaGetErrorString(err));
-
-	// calculate D
-
-	// G1(:,i) invoke getCol kernel
-	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-	dim3 dimGrid( (d_G1.width + dimBlock.x - 1)/dimBlock.x, (d_G1.height + dimBlock.y - 1)/dimBlock.y );
-	getColKernel<<<dimGrid, dimBlock>>>(d_G1, d_G1_col, y);
-	
-	// G2t(j,:) invoke getRow kernel
-	dimGrid = dim3( (d_G2t.width + dimBlock.x - 1)/dimBlock.x, (d_G2t.height + dimBlock.y - 1)/dimBlock.y );
-	getRowKernel<<<dimGrid, dimBlock>>>(d_G2t, d_G2t_row, x);
-
-	// calculate d_D
-	dimGrid = dim3( (d_Y.width + dimBlock.x - 1)/dimBlock.x, (d_Y.height + dimBlock.y - 1)/dimBlock.y ); 
-	expDkernel<<<dimGrid, dimBlock>>>(d_G1_col, d_G2t_row, d_Y, sigma);	
-
-	// free memory space
-	cudaFree(d_G1_col.elements);
-	cudaFree(d_G2t_row.elements);
-
-}
-	
-__global__
-void marginalize1(Matrix d_G1, Matrix d_G2t, float sigma, Matrix d_Y) {
-	int y = blockIdx.y * blockDim.y + threadIdx.y;
-	int x = blockIdx.x * blockDim.x + threadIdx.x;
-	// if out of boundary return
-	if (y >= d_Y.height || x >= d_Y.width) return;
-	
-	// create d_D
-	Matrix d_D;
-	d_D.height = d_Y.height;
-	d_D.width = d_Y.width;
-	size_t size = d_D.width * d_D.height * sizeof(float);
-	cudaError_t err = cudaMalloc(&d_D.elements, size);
-	printf("CUDA malloc d_D: %s\n", cudaGetErrorString(err));
-	
-	// create d_G1_col and d_G2_row	
-	Matrix d_G1_col, d_G2t_row;
-	d_G1_col.height = d_G1.height;
-	d_G1_col.width = 1;
-	size = d_G1_col.width * d_G1_col.height * sizeof(float);
-	err = cudaMalloc(&d_G1_col.elements, size);
-	printf("CUDA malloc d_G1_col: %s\n", cudaGetErrorString(err));
-	d_G2t_row.height = 1;
-	d_G2t_row.width = d_G2t.width;
-	size = d_G2t_row.width * d_G2t_row.height * sizeof(float);
-	err = cudaMalloc(&d_G2t_row.elements, size);
-	printf("CUDA malloc d_G2t_row: %s\n", cudaGetErrorString(err));
-
-	// calculate D
-
-	// G1(:,i) invoke getCol kernel
-	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-	dim3 dimGrid( (d_G1.width + dimBlock.x - 1)/dimBlock.x, (d_G1.height + dimBlock.y - 1)/dimBlock.y );
-	getColKernel<<<dimGrid, dimBlock>>>(d_G1, d_G1_col, y);
-	
-	// G2t(j,:) invoke getRow kernel
-	dimGrid = dim3( (d_G2t.width + dimBlock.x - 1)/dimBlock.x, (d_G2t.height + dimBlock.y - 1)/dimBlock.y );
-	getRowKernel<<<dimGrid, dimBlock>>>(d_G2t, d_G2t_row, x);
-
-	// calculate d_D
-	dimGrid = dim3( (d_D.width + dimBlock.x - 1)/dimBlock.x, (d_D.height + dimBlock.y - 1)/dimBlock.y ); 
-	calculateD<<<dimGrid, dimBlock>>>(d_G1_col, d_G2t_row, d_D);	
-	
-	// exp((-d.*d)./sigma) invoke exp kernel
-	expKernel<<<dimGrid, dimBlock>>>(d_D, sigma);
-	
-	// write to Y
-	matAddKernel<<<dimGrid, dimBlock>>>(d_Y, d_D, d_Y);
-
-	// free memory space
-	cudaFree(d_G1_col.elements);
-	cudaFree(d_G2t_row.elements);
-	cudaFree(d_D.elements);
-
-}
-
-
-__global__
-void marginalize0(Matrix d_G1, Matrix d_G2t, float sigma, Matrix d_Y) {
-	int y = blockIdx.y * blockDim.y + threadIdx.y;
-	int x = blockIdx.x * blockDim.x + threadIdx.x;
-	// if out of boundary return
-	if (y >= d_Y.height || x >= d_Y.width) return;
-
-	// create d_G1_col and d_G2_row	
-	Matrix d_G1_col, d_G2t_row;
-	d_G1_col.height = d_G1.height;
-	d_G1_col.width = 1;
-	size_t size = d_G1_col.width * d_G1_col.height * sizeof(float);
-	cudaError_t err = cudaMalloc(&d_G1_col.elements, size);
-	printf("CUDA malloc d_G1_col: %s\n", cudaGetErrorString(err));
-	d_G2t_row.height = 1;
-	d_G2t_row.width = d_G2t.width;
-	size = d_G2t_row.width * d_G2t_row.height * sizeof(float);
-	err = cudaMalloc(&d_G2t_row.elements, size);
-	printf("CUDA malloc d_G2t_row: %s\n", cudaGetErrorString(err));
-	
-	
-	// calculate D
-
-	// G1(:,i) invoke getCol kernel
-	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-	dim3 dimGrid( (d_G1.width + dimBlock.x - 1)/dimBlock.x, (d_G1.height + dimBlock.y - 1)/dimBlock.y );
-	getColKernel<<<dimGrid, dimBlock>>>(d_G1, d_G1_col, y);
-	
-	// G2t(j,:) invoke getRow kernel
-	dimGrid = dim3( (d_G2t.width + dimBlock.x - 1)/dimBlock.x, (d_G2t.height + dimBlock.y - 1)/dimBlock.y );
-	getRowKernel<<<dimGrid, dimBlock>>>(d_G2t, d_G2t_row, x);
-
-	// create d_D, d_D1, d_D2
-	Matrix d_D, d_D1, d_D2;
-	d_D.height = d_D1.height = d_D2.height = d_Y.height;
-	d_D.width = d_D1.width = d_D2.width = d_Y.width;
-	size = d_D.width * d_D.height * sizeof(float);
-	err = cudaMalloc(&d_D.elements, size);
-	printf("CUDA malloc d_D: %s\n", cudaGetErrorString(err));
-	size = d_D1.width * d_D1.height * sizeof(float);
-	err = cudaMalloc(&d_D1.elements, size);
-	printf("CUDA malloc d_D1: %s\n", cudaGetErrorString(err));
-	size = d_D2.width * d_D2.height * sizeof(float);
-	err = cudaMalloc(&d_D2.elements, size);
-	printf("CUDA malloc d_D2: %s\n", cudaGetErrorString(err));
-
-
-	// repmat(G1(:,i),1,n2) invoke repmat kernel
-	dimGrid = dim3( (d_G1_col.width + dimBlock.x - 1)/dimBlock.x, (d_G1_col.height + dimBlock.y - 1)/dimBlock.y );
-	repmatKernel<<<dimGrid, dimBlock>>>(d_G1_col, 1, d_G2t.width, d_D1);
-
-	// repmat(G2t(j,:),n1,1) invoke repmat kernel
-	dimGrid = dim3( (d_G2t_row.width + dimBlock.x - 1)/dimBlock.x, (d_G2t_row.height + dimBlock.y - 1)/dimBlock.y );
-	repmatKernel<<<dimGrid, dimBlock>>>(d_G2t_row, d_G1.height, 1, d_D2);
-
-	// d_D1 - d_D2 invoke matSub kernel
-	dimGrid = dim3( (d_D.width + dimBlock.x - 1)/dimBlock.x, (d_D.height + dimBlock.y - 1)/dimBlock.y );
-	matSubKernel<<<dimGrid, dimBlock>>>(d_D1, d_D2, d_D);
-
-	// exp((-d.*d)./sigma) invoke exp kernel
-	expKernel<<<dimGrid, dimBlock>>>(d_D, sigma);
-	
-	// write to Y
-	matAddKernel<<<dimGrid, dimBlock>>>(d_Y, d_D, d_Y);
-
-	// free memory space
-	cudaFree(d_G1_col.elements);
-	cudaFree(d_G2t_row.elements);
-	cudaFree(d_D.elements);
-	cudaFree(d_D1.elements);
-	cudaFree(d_D2.elements);
-	
-}
+void marginalize(Matrix, Matrix, float, Matrix); 
 
 void graphMatching(Matrix G1, Matrix G2, float sigma, int numberOfMatches, Matrix X, Matrix Z, Matrix Y) {
 	/****************************************************************************************	
@@ -304,3 +104,23 @@ void graphMatching(Matrix G1, Matrix G2, float sigma, int numberOfMatches, Matri
 	// call hypergraphMatching()
 	hypergraphMatching(Y, numberOfMatches, X, Z);
 }
+
+__global__
+void marginalize(Matrix d_G1, Matrix d_G2t, float sigma, Matrix d_Y) {
+	int i = blockIdx.y * blockDim.y + threadIdx.y;
+	int j = blockIdx.x * blockDim.x + threadIdx.x;
+	// if out of boundary return
+	if (i >= d_Y.height || j >= d_Y.width) return;
+
+	for (int r=0; r < d_Y.height; r++) {
+		for (int c=0; c < d_Y.width; c++) {
+			float d = d_G1.elements[r * d_G1.width + i] - d_G2t.elements[j * d_G2t.width + c];
+			float e = exp(-d * d / sigma);
+			//d_Y.elements[r * d_Y.width + c] += e;
+			atomicAdd(d_Y.elements + r * d_Y.width + c, e);
+		}
+	}
+	
+}
+
+
